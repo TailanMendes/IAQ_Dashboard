51a9e4e48460523cf18604e268acbdbb4c36f933
Tailan Mendes <tailan.cardoso.eng@gmail.com>
2022-08-07 15:09:06 -0400

    Criado arquivo iaq_data.ms para armazenamento dos dados do contrato

--- IAQ_Dashboard/BCInterface.cs
+++ IAQ_Dashboard/BCInterface.cs
@@ -16,8 +16,8 @@ namespace IAQ_Dashboard
     {
         public async void getContractData()
         {
-            var web3 = new Web3("https://rinkeby.infura.io/v3/ea9cd517fe4c4782a5f7ee526a578ec5");
 
+            var web3 = new Web3("https://rinkeby.infura.io/v3/ea9cd517fe4c4782a5f7ee526a578ec5");
             var contractAddress = "0x194E4A079f507208B544eB8EAd2821e3Daa6a8C5";
             var contractHandler = web3.Eth.GetContractHandler(contractAddress);
 
@@ -25,8 +25,50 @@ namespace IAQ_Dashboard
 
             var getMeasureFunctionReturn = await contractHandler.QueryAsync<GetMeasureFunction, List<string>>();
 
+            storeDataIntoFile(getMeasureFunctionReturn);
+
             var message = string.Join(Environment.NewLine, getMeasureFunctionReturn);
-            MessageBox.Show(message);
+            //MessageBox.Show(message);
+        }
+
+        private void storeDataIntoFile(List<string> getMeasureFunctionReturn)
+        {            
+            string data_path = @"c:\\iaq_data.ms";
+
+            // If file doesnt exists insert all data
+            if (!File.Exists(data_path))
+            {
+                File.AppendAllLines(data_path, getMeasureFunctionReturn);
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(data_path);
+                var last_line = File.ReadLines(data_path).Last();
+                sr.Close();
+
+                UInt32 last_epoch_time = Convert.ToUInt32(getEpochTime(last_line));
+
+                foreach (string s in getMeasureFunctionReturn)
+                {
+                    UInt32 epoch = Convert.ToUInt32(getEpochTime(s));
+
+                    if (epoch > last_epoch_time)
+                    {
+                        File.AppendAllText(data_path, s + Environment.NewLine);
+
+                    }
+
+                }
+            }
+
+            //File.AppendAllLines(data_path, getMeasureFunctionReturn);                 
+        }
+
+        private string getEpochTime(string s)
+        {
+            string[] subs = s.Split('|', 3);
+            var epochtime = subs[2];
+            return epochtime;
         }
 
         public partial class GetMeasureFunction : GetMeasureFunctionBase { }
--- IAQ_Dashboard/frmDashboard.cs
+++ IAQ_Dashboard/frmDashboard.cs
@@ -14,15 +14,15 @@ namespace IAQ_Dashboard
         {
             this.bcInt = new BCInterface();
 
-            InitializeTimer();
+                InitializeTimer();
 
         }
 
         private void InitializeTimer()
         {
             // Call this procedure when the application starts.  
-            // Set to 5 second.  
-            timer1.Interval = 5000;
+            // Set to 10 second.  
+            timer1.Interval = 10000;
             timer1.Tick += new EventHandler(DownloadData);
 
             
